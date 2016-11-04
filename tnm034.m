bf900b795ec30e1798a07c78ead80156bc954a78
johanhenrikssn <johan.c.henriksson@gmail.com>
2016-11-04 14:31:22 +0100

    Rotate input image using hough transform

--- tnm034.m
+++ tnm034.m
@@ -17,20 +17,26 @@ close all
 
 image = imread('images/im1s.jpg');
 
-%imshow(image);
-
-
 %% STAFF LINE IDENTIFICATION 
 
-% Horizontal projection
-
+% Binary image
 bw = 1-im2bw(image, 0.8);
+
+% Find rotation using Hough transform
+[H, theta, rho] = hough(bw, 'theta', -89.9:0.1:89.9);
+peak = houghpeaks(H);
+barAngle = theta(peak(2));
+B = imrotate(image, 90 + barAngle, 'loose');
+bw = 1-im2bw(B, 0.8);
+
+% Find locations using Horizontal projection
 figure
 plot(sum(bw,2), fliplr(1:size(bw,1)));
 [pks, locs] = findpeaks(sum(bw,2));
 
-% Remove all peaks less than half of maximum peak
-tresh = pks > max(pks)/2;
+% Remove all unrelevant peaks based on threshold
+% Can be improved by using cluster classification
+tresh = pks > max(pks)/3;
 locs = locs .* tresh;
 pks = pks .* tresh;
 
@@ -38,10 +44,8 @@ pks_tresh = pks(pks~=0);
 locs_tresh = locs(locs~=0);
 cluster_i = kmeans(locs_tresh, 3);
 
-%cluster = [locs_tresh, pks_tresh, cluster_i];
-
 imshow(bw)
-    hold on
+hold on
 
 % Print located staff lines
 for i = 1:length(locs_tresh)
