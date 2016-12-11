5852a885a1116686a5bd8d56c6ad5ba5d1bbdf69
johanhenrikssn <johan.c.henriksson@gmail.com>
2016-12-11 13:07:29 +0100

    Renaming

--- rotate_image.m
+++ rotate_image.m
@@ -6,14 +6,14 @@ function [ rotated_image ] = rotate_image( image )
 
     [H, theta, rho] = hough(image, 'theta', -89.9:0.1:89.9);
     peak = houghpeaks(H);
-    barAngle = theta(peak(2));
+    lineAngle = theta(peak(2));
 
-    if barAngle > 0
-        angle = 270 + barAngle;
+    if lineAngle > 0
+        rotationAngle = 270 + lineAngle;
     else
-        angle = 90 + barAngle;
+        rotationAngle = 90 + lineAngle;
     end
     
-     rotated_image = imrotate(image, angle, 'bilinear','crop');
+     rotated_image = imrotate(image, rotationAngle, 'bilinear','crop');
 end
 
